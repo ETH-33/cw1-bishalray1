39c07f91b1ed764e9a9fc1b7191871c08af404f7
Bishal Kumar Ray <bishalray2002@gmail.com>
2023-07-15 09:47:54 +0545

    index or home page of website

---
+++ index.php
@@ -0,0 +1,417 @@
+<?php include('include/header.php') ?>
+
+<!-- Carousel  -->
+  
+  <section class="carousel-section" id="slider">
+   
+   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+       <ol class="carousel-indicators">
+         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+       </ol>
+       <div class="carousel-inner">
+         <div class="carousel-item active">
+           <img class="d-block w-100" src="assets/picture/cesourel/B.jpg" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+               <h1 class="display-5">Radiant Illumination</h1>
+               <p>Elevate your hotel's online presence with our captivating light effect. This stunning visual feature casts a warm and welcoming glow, creating an inviting ambiance that entices potential guests.</p>
+             </div>
+         </div>
+         <div class="carousel-item ">
+           <img class="d-block w-100" src="assets/picture/cesourel/location.jpg" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+               <h1 class="display-5 text-dark">Well-connected roads and transportation system</h1>
+               <p class="text-dark">Discover a well-connected network of roads and transportation systems that seamlessly connect cities, towns, and destinations. Our region boasts a modern and efficient infrastructure, designed to facilitate easy travel and efficient logistics.</p>
+             </div>
+         </div>
+         <div class="carousel-item ">
+           <img class="d-block w-100" src="assets/picture/cesourel/Parking.jpg" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+               <h1 class="display-5 text-dark">Ample Parking Space</h1>
+               <p class="text-dark">Our facility offers a generous and well-designed parking area with sufficient capacity to accommodate a large number of vehicles. You can enjoy the convenience of easily finding a spot to park your car without any hassle.</p>
+             </div>
+         </div>
+         <div class="carousel-item ">
+           <img class="d-block w-100" src="assets/picture/cesourel/Reception.jpg" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+               <h1 class="display-5">Welcoming Experience</h1>
+               <p >Easy accessibility to commercial points and key areas in the city with best parking area.</p>
+             </div>
+         </div> 
+         <div class="carousel-item ">
+           <img class="d-block w-100" src="assets/picture/cesourel/Service.jpg" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+               <h1 class="display-5 text-dark"> Guest can eat and drink within their own private room in the hotel</h1>
+               <p class="text-dark">Offer coupons and discounts to local attractions...</p>
+             </div>
+         </div>
+           <div class="carousel-item ">
+           <img class="d-block w-100" src="assets/picture/cesourel/lighting.jpg" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+               <h1 class="display-5">The Best lighting Effects </h1>
+               <p>The quality of the overall lighting scheme and its effect on mood 
+                 is often dependent on the relationship between the key and fill lights...</p>
+             </div>
+         </div>
+       </div>
+       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="sr-only">Previous</span>
+       </a>
+       <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="sr-only">Next</span>
+       </a>
+     </div>
+   </section>
+
+ 
+ <!-- Page Content -->
+
+ <section class="py-5 white-section" id="introduction" >
+   <div class="container">
+     <h1 class="display-4 title"><?php echo $general_setting['Name'] ?>,luxury collections ,Lahan</h1>
+     <p class="lead">A premier accommodation offering stylish rooms, excellent amenities, and exceptional service for an unforgettable stay.</p>
+   </div>
+
+   <div class="container"> 
+       
+           <h3>Overview</h3>
+     
+           <p class="text-justify">Bishal Hotel is a premier destination for travelers seeking a luxurious and memorable stay. Located in a prime location, the hotel offers stylishly designed rooms, top-notch amenities, and exceptional service. With a range of facilities including a fitness center, spa, and on-site restaurant, Bishal Hotel promises a comfortable and indulgent experience for all guests.</p>
+
+               <p><blockquote class="blockquote">Ideal Location</blockquote>
+
+               Bishal Hotel enjoys an ideal location that sets it apart as a prime destination for travelers. Situated in a vibrant and convenient area, the hotel offers easy access to key attractions, business centers, and transportation hubs. Whether guests are exploring the city's landmarks or attending important meetings, Bishal Hotel's central location ensures a seamless and efficient experience. Additionally, the surrounding neighborhood is known for its bustling energy, diverse dining options, and shopping opportunities, further enhancing the overall convenience and appeal of Bishal Hotel's location.
+               </p>
+                       
+               <p>     
+               <blockquote class="blockquote">Refined Artistry at Bishal Hotel</blockquote>
+               Immerse yourself in a world of elegant sophistication and impeccable craftsmanship. From the meticulously curated artwork adorning the walls to the intricately designed furnishings, Bishal Hotel embodies a true masterpiece of refined artistry. Every detail has been thoughtfully considered, creating a harmonious blend of aesthetics and comfort that elevates the guest experience to new heights. Prepare to be enchanted by the exquisite fusion of artistic expression and luxurious accommodations at Bishal Hotel.
+               </p>
+               
+      </div>       
+   </section>    
+   
+   <!--Hotel Features-->
+       
+   <section class="py-5 colored-section" id="grid" >
+       
+           <div class="container">
+           <div class="row">
+               
+               <div class="col-xs-12 col-sm-6 col-md-4 cd-lg-4">
+               <div class="box container text-center">
+                  <img class='img mb-4' src='assets/picture/common/summing.jpg' alt='Image Error' width='350px' height='200px'>
+                       <center><h3>CITY GETAWAY</h3></center>
+                   <p class="mb-4">Discover exquisite weekend getaways in the comfort of your own city. Enjoy breakfast,
+                        Wi-Fi, happy hours, savings on dining & spa, and more.</p>
+                      
+               </div>
+               </div>
+               
+               <div class="col-xs-12 col-sm-6 col-md-4 cd-lg-4">
+               <div class="box container text-center">
+                   <img class='img mb-4' src='assets/picture/common/accomadation.jpg' alt='Image Error'  width='350px' height='200px'>
+                   <center><h3>ACCOMMODATION</h3></center>
+                   <p class="mb-4">With 506 rooms, 78 luxuriously appointed service apartments & 16 palatial suites which
+                        are the epitome of Indian grace and style.</p>
+           
+               </div>
+               </div>
+               
+               <div class="col-xs-12  col-sm-6 col-md-4 cd-lg-4">
+               <div class="box container text-center">
+                   <img class='img mb-4' src='assets/picture/common/well.jpg' alt='Image Error'  width='350px' height='200px'> 
+                   <center><h2>DINING</h2></center>
+                   <p class="mb-4">Ten distinct dining destinations featuring Indian & international cuisine along with some of the world's most well appreciated and prized beverages.</p>
+                   
+               </div>
+               </div>
+             </div>
+               <div class="row">
+               
+                   <div class="col-xs-12  col-sm-6 col-md-4 cd-lg-4">
+                   <div class="box container text-center">
+                       <img class='img mb-4' src='assets/picture/common/dining.jpg' alt='Image Error' width='350px' height='200px'>
+           
+                       <center><h2>WELLNESS</h2></center>
+                       <p class="mb-4">Embodying our commitment to holistic wellbeing, ITC Grand Chola offers facilities that provide healthier options for a better stay.</p>
+                       
+                   </div>
+                   </div>
+                   
+                   <div class="col-xs-12 col-sm-6 col-md-4 cd-lg-4">
+                   <div class="box container text-center">
+                       <img class='img mb-4' src='assets/picture/common/suits.jpg' alt='Image Error' width='350px' height='200px'>
+                       <center><h2>SHORT BREAK</h2></center>
+                       <p class="mb-4">Capture a lifetime of great memories. Includes Wi-Fi, Breakfast and more</p>
+               
+                   </div>
+                   </div>
+                   
+                   <div class="col-xs-12  col-sm-6 col-md-4 cd-lg-4">
+                   <div class="box container text-center">
+                       <img class='img mb-4' src='assets/picture/common/away.jpg' alt='Image Error' width='350px' height='200px'>
+                       <center><h2>SUITE SOJOURNS</h2></center>
+                        <p class="mb-4">Exceptional value for a superlative suite experience with airport transfers, Wi-Fi, breakfast and more.</p>
+                  
+                   </div>
+                   </div>
+               
+           </div>
+          
+
+
+    </section>
+
+
+
+   
+    <section class="white-section" id="feature">
+    <div class="row featurette m-5">
+          <div class="col-md-7 container">
+            <h1 class="featurette-heading display-5">Managing Hotel Facility. <span class="text-muted lead">Space Usage Plan</span></h1>
+            <p class="lead">Management of a hotel facility is another important aspect of facility planning that facility planning firms, such as HPG Consulting, handle on a regular basis. The main objective is to ensure seamless coordination among various hotel processes, and business continuity and sustainability. It takes into account hotel space, infrastructure, personnel,
+             and equipment and helps ensure they comply with the requirements of the business.</p>
+          </div>
+          <div class="col-md-5 ">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 400px;" src="assets/picture/service/space.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+</section>
+
+<!-- Testimonials -->
+  
+
+<section class="colored-section pt-3" id="testiminoal-rooms">
+
+<div class="container my-4">
+
+<!--Carousel Wrapper-->
+<div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+  <!--Controls-->
+  <div class="controls-top d-flex justify-content-center">
+    <a class="btn-floating" href="#multi-item-example" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
+    <a class="btn-floating ml-5" href="#multi-item-example" data-slide="next"><i
+        class="fas fa-chevron-right"></i></a>
+  </div>
+  <!--/.Controls-->
+
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
+    <li data-target="#multi-item-example" data-slide-to="1"></li>
+    
+  </ol>
+  <!--/.Indicators-->
+
+  <!--Slides-->
+  <br>
+  <div class="carousel-inner" role="listbox">
+
+    <!--First slide-->
+    <div class="carousel-item active">
+
+      <div class="col-md-3" style="float:left">
+       <div class="card mb-2 ">
+          <img class="card-img-top"
+            src="assets/picture/common/away.jpg" alt="Card image cap" height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Suite Sojourns</h4>
+            <p class="card-text">Exceptional value for a superlative experience with Wi-Fi, breakfast and more.</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2">
+          <img class="card-img-top"
+            src="assets/picture/common/suits.jpg" alt="Card image cap" height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Short Break</h4>
+            <p class="card-text">Capture a lifetime of great memories. 
+              Includes Wi-Fi, Breakfast and more</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2">
+          <img class="card-img-top"
+            src="assets/picture/common/dining.jpg" alt="Card image cap" height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Wellness</h4>
+            <p class="card-text">Capture a lifetime of great memories.
+               Includes Wi-Fi, Breakfast and more</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      
+       <div class="col-md-3" style="float:left">
+       <div class="card mb-2">
+          <img class="card-img-top"
+            src="assets/picture/common/accomadation.jpg" alt="Card image cap" height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Accommodation</h4>
+            <p class="card-text"> Apartments which
+                        are the epitome of Indian grace and style</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!--/.First slide-->
+
+    <!--Second slide-->
+    <div class="carousel-item">
+
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2">
+          <img class="card-img-top"
+            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap"  height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">City Gateway</h4>
+            <p class="card-text">Discover exquisite weekend getaways in the comfort of your own city. Enjoy breakfast,
+                       savings on dining & spa, and more</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2">
+          <img class="card-img-top"
+            src="assets/picture/common/events.jpeg" alt="Card image cap"  height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Conference</h4>
+            <p class="card-text">Ten distinct dining destinations featuring Indian & international 
+              cuisine along with well appreciated and prized beverages</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2">
+          <img class="card-img-top"
+            src="assets/picture/common/wedding.jpg" alt="Card image cap" height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Wedding</h4>
+            <p class="card-text">Dream up a traditional Indian wedding and turn it into
+               an event extraordinaire. Encapsulate the memorable moments into reality</p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2">
+          <img class="card-img-top"
+            src="assets/picture/common/meeting.jpeg" alt="Card image cap" height="180" width="50">
+          <div class="card-body">
+            <h4 class="card-title">Meeting</h4>
+            <p class="card-text">The Killi, Kaveri and Tanjore meeting rooms are the perfect combination
+               of space and audio visual equipments
+            </p>
+            <a class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!--/.Second slide-->
+
+   
+
+  </div>
+  <!--/.Slides-->
+
+</div>
+<!--/.Carousel Wrapper-->
+</section>
+
+<section id="contact" class="white-section">
+<div class="container-xl mb-5 p-5">
+	<div class="row">
+		<div class="col-md-8 mx-auto">
+		<div class="message"></div>
+			<div class="contact-form">
+				<h1>Contact Us</h1>
+				<p class="hint-text">We'd love to hear from you, please drop us a line if you've any query.</p>
+				<form id="contact-form" action="functions.php" method="post" autocomplete="off">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="inputFirstName">First Name</label>
+								<input type="text" class="form-control" id="FirstName" name="FirstName" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="inputLastName">Last Name</label>
+								<input type="text" class="form-control" id="LastName" name="LastName" required>
+							</div>
+						</div>
+					</div>            
+					<div class="form-group">
+						<label for="inputEmail">Email Address</label>
+						<input type="email" class="form-control" id="Email" name="Email" required>
+					</div>
+					<div class="form-group">
+						<label for="inputMessage">Message</label>
+						<textarea class="form-control" id="Message" name="Message" rows="5" required></textarea>
+					</div>
+					<button type="submit" class="btn btn-primary" name = "contact" > Submit </button>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+<script>
+	$(document).ready(function(){
+	$('#contact-form').on('submit',function(e){
+	  e.preventDefault();
+      var formData = new FormData(this);
+
+      $.ajax({
+        url:"functions.php",
+        type:"POST",
+        data:formData,
+        cache:false,
+        contentType: false,
+        processData: false,
+        success:function(data){      
+			console.log("data");
+		  console.log(data);
+          var json = JSON.parse(data);
+          if(json['error']!=""){
+             $('.message').html(`<div class="alert alert-danger" role="alert"> ${json['error']}  </div>`);
+			 $('#contact-form')[0].reset();
+          }else{
+			$('.message').html(`<div class="alert alert-success" role="alert"> ${json['msg']}  </div>`);
+			$('#contact-form')[0].reset();
+          }
+          
+           
+       },
+       error: function(data){
+           console.log("error");
+           console.log(data);
+       }
+      });
+		})
+	})
+</script>
+
+    
+    <?php include('include/footer.php') ?>
