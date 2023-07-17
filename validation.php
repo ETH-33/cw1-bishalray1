e6dd6c0f41cdc590e18bf1ce46b4e147613535a9
Bishal Kumar Ray <bishalray2002@gmail.com>
2023-07-17 15:17:54 +0545

    valadation for signup and login

--- validation.php
+++ validation.php
@@ -2,33 +2,34 @@
 
 include('include/functions.php');
 
-if(isset($_POST['user_login'])){
+if (isset($_POST['user_login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $select_user="select * from  users_details where Email='$email' && Password ='$password'";
-    $result=mysqli_query($con,$select_user) or die ('failed to query');
-    $row=mysqli_fetch_array($result);
-    
-    if(($row['Email']==$email) && ($row['Password']==$password))
-    {
-        if($row['Email']=="admin@gmail.com"){
-            header("Location:admin/dashboard.php?");
-        } 
-        else 
-        {
-            header("Location:index.php?");
+    $select_user = "SELECT * FROM users_details WHERE Email='$email'";
+    $result = mysqli_query($con, $select_user) or die('Failed to query');
+    $row = mysqli_fetch_array($result);
+
+    if ($row) {
+        $hashedPassword = $row['Password'];
+
+        // Verify the password using password_verify()
+        if (password_verify($password, $hashedPassword)) {
+            if ($row['Email'] == "admin@gmail.com") {
+                header("Location: admin/dashboard.php");
+            } else {
+                header("Location: index.php");
+            }
+
+            $_SESSION['loggedUserName'] = $row['FirstName'];
+            $_SESSION['loggedUserId'] = $row['UserId'];
+        } else {
+            $error = "Invalid Username and Password!";
+            error('login.php', $error);
         }
-
-        $_SESSION['loggedUserName']=$row['FirstName'];
-        $_SESSION['loggedUserId']=$row['UserId'];
-       
-        
-            
+    } else {
+        $error = "Invalid Username and Password!";
+        error('login.php', $error);
     }
-    else{
-        $error="Invalid Username and Password !";
-        error('login.php',$error);
-    } 
 }
 ?>
